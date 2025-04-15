1af255903428204eb1b19cc814336ba85bf0ed24
Rahul Rathod <rathodrahul1705@gmail.com>
2025-04-15 11:43:23 +0530

    minor changes

--- BizEats/settings.py
+++ BizEats/settings.py
@@ -75,12 +75,20 @@ DATABASES = {
 RAZORPAY_API_KEY = config('RAZORPAY_API_KEY')
 RAZORPAY_API_SECRET = config('RAZORPAY_API_SECRET')
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.hostinger.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = config('EMAIL_HOST')  # e.g., 'smtp.hostinger.com'
+EMAIL_PORT = config('EMAIL_PORT', cast=int)  # usually 587
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)  # usually True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # your full email
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # your email password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 AUTH_USER_MODEL = 'api.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
