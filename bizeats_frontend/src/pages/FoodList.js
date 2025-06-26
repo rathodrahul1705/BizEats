3ceb4e01ea6474c0edf701463436678bb6a58f57
Rahul Rathod <rathodrahul1705@gmail.com>
2025-06-26 08:51:18 +0530

    minor changes

--- bizeats_frontend/src/pages/FoodList.js
+++ bizeats_frontend/src/pages/FoodList.js
@@ -99,10 +99,6 @@ const FoodGrid = () => {
           
           <div className="split_card__footer">
             <span className="split_card__location">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                <circle cx="12" cy="10" r="3"></circle>
-              </svg>
               {restaurant.restaurant_location}
             </span>
           </div>
