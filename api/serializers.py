7fdf8ef8729462402a1cf4683434cb4eaceaa9c7
Rahul Rathod <rathodrahul1705@gmail.com>
2025-06-29 10:20:54 +0530

    mibopr changes

--- api/serializers.py
+++ api/serializers.py
@@ -90,7 +90,7 @@ class RestaurantMasterSerializer(serializers.ModelSerializer):
         restaurant_master = RestaurantMaster.objects.create(
             restaurant_name=validated_data['restaurant_name'],
             restaurant_id=restaurant_id,
-            restaurant_status=2,  # Default 'Active' status
+            restaurant_status=1,  # Default 'Active' status
             user=user  # Authenticated user
         )
 
