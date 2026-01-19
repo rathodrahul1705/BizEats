393aaeb4aa72ca245faa5fff4c4aaa68b80b858a
Rahul Rathod <rathodrahul1705@gmail.com>
2026-01-19 16:40:51 +0530

    changes

--- BizEats/settings.py
+++ BizEats/settings.py
@@ -27,6 +27,8 @@ ALLOWED_HOSTS = ['www.eatoor.com', 'eatoor.com','*']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://eatoor.com",
+    "https://www.eatoor.com",
 ]
 
 INSTALLED_APPS = [
