49b922f1d89bca71cacd0a9bc3a450ca853c0236
Rahul Rathod <rathodrahul1705@gmail.com>
2026-04-15 19:52:47 +0530

    changes

--- api/payu/views.py
+++ api/payu/views.py
@@ -586,7 +586,7 @@ def payment_method_details(request):
                 "method_id": PAYMENT_METHOD_MAP["wallet"],
                 "id": "eatoor_money",
                 "name": "Eatoor Money",
-                "icon": "https://eatoorprod.s3.eu-north-1.amazonaws.com/logo/paymt_image.webp",
+                "icon": "https://eatoorprod.s3.eu-north-1.amazonaws.com/logo/eatoormob.png",
                 "balance": 0,
                 "is_active": True
             }
