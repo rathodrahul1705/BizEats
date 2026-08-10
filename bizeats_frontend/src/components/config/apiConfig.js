59458ded63923c0ebd17b5ff20913b7cdc14000f
Rahul Rathod <46216499+rathodrahul1705@users.noreply.github.com>
2026-08-10 17:49:14 +0530

    commit

--- bizeats_frontend/src/components/config/apiConfig.js
+++ bizeats_frontend/src/components/config/apiConfig.js
@@ -120,10 +120,10 @@ const API_ENDPOINTS = {
   },
 
   OFFER: {
-    CREATE: `${BASE_URL}/offers/`,
-    FETCH: (restaurant_id) => `${BASE_URL}/offers/?restaurant_id=${restaurant_id}`,
-    DELETE: (coupon_id) => `${BASE_URL}/offers/${coupon_id}/`,
-    UPDATE: (coupon_id) => `${BASE_URL}/offers/${coupon_id}/`
+    CREATE: `${BASE_URL}/offer/`,
+    FETCH: (restaurant_id) => `${BASE_URL}/offer/?restaurant_id=${restaurant_id}`,
+    DELETE: (coupon_id) => `${BASE_URL}/offer/${coupon_id}/`,
+    UPDATE: (coupon_id) => `${BASE_URL}/offer/${coupon_id}/`
   },
 
   PROFILE: {
